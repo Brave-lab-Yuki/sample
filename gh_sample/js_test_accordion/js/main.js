0057e82d7848964b57afb18a046e2cc45d41e3d3
Brave-lab-Yuki <hiroo914.28@gmail.com>
2022-03-08 13:05:00 +0900

    sampleを追加

---
+++ gh_sample/js_test_accordion/js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+{
+    const dts = document.querySelectorAll('dt');
+    console.log(dts);
+    dts.forEach(dt => {
+        console.log(dt);
+        dt.addEventListener('click', () => {
+            dt.parentNode.classList.toggle('appear');
+
+            dts.forEach(el => {
+                if (dt !== el) {
+                    el.parentNode.classList.remove('appear');
+                }
+            });    
+        });
+    });
+}
